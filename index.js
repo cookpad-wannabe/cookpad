7182fb1261f967a34877e35490aa9d89affc69d0
Resha Puspita <reshapuspita734@gmail.com>
2020-07-17 20:38:02 +0700

    redirect home

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.use(expressLayout);
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.send({ message: "Welcome to my API with passport" });
+  res.redirect("http://localhost:5005/users/register");
 });
 app.use("/users", require("./routes/users"));
 app.use("/recipes", require("./routes/recipes"));
